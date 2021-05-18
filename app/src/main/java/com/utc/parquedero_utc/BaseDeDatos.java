d4d517194791fd77d30ff49059d36ff5e039aacc
kathy <katherine.rovayo9809@utc.edu.ec>
2021-05-18 17:22:47 -0500

    se guarda la fecha y hora de registro

--- app/src/main/java/com/utc/parquedero_utc/BaseDeDatos.java
+++ app/src/main/java/com/utc/parquedero_utc/BaseDeDatos.java
@@ -14,7 +14,7 @@ public class BaseDeDatos extends SQLiteOpenHelper {
     private static final String nombreBdd="bdd_parqueadero_utc"; // definir el nombre de la base de datos
     private static final int versionBdd=1; // definir la version de la base de datos
     private static final String tablaUsuario="create table usuario(id_usu integer primary key autoincrement, nombre_usu text," +
-            "apellido_usu text, email_usu text,telefono_usu text, direccion_usu txt,password_usu text)";//definir la estructura de la tabla de usuarios
+            "apellido_usu text, email_usu text,telefono_usu text, direccion_usu txt,password_usu text,fecha_regis_usu text)";//definir la estructura de la tabla de usuarios
     //contructor
     public BaseDeDatos (Context contexto){
         super(contexto, nombreBdd, null, versionBdd);
@@ -31,11 +31,11 @@ public class BaseDeDatos extends SQLiteOpenHelper {
         db.execSQL(tablaUsuario); // ejecucion del codigo para crear la tabla usuario con nueva estructura
     }
     // proceso 3
-    public boolean agregarUsuario(String nombre, String apellido, String email, String telefono, String direccion ,String password){
+    public boolean agregarUsuario(String nombre, String apellido, String email, String telefono, String direccion ,String password, String fecha_regis_usu){
         SQLiteDatabase miBdd = getWritableDatabase(); // llamando a la base de datos en el objeto miBdd
         if(miBdd!=null){// validando que existan datos en la vase de datos
-            miBdd.execSQL("insert into usuario(nombre_usu,apellido_usu, email_usu,telefono_usu, direccion_usu, password_usu) " +
-                    "values('"+nombre+"','"+apellido+"','"+email+"','"+telefono+"','"+direccion+"','"+password+"')");//ejecutando la sentencia de insercion sql
+            miBdd.execSQL("insert into usuario(nombre_usu,apellido_usu, email_usu,telefono_usu, direccion_usu, password_usu,fecha_regis_usu) " +
+                    "values('"+nombre+"','"+apellido+"','"+email+"','"+telefono+"','"+direccion+"','"+password+"','"+fecha_regis_usu+"')");//ejecutando la sentencia de insercion sql
             miBdd.close(); //cerrando la conexion a la base de datos
             return true; //retorno cuando la insercion es exitosa
         }
